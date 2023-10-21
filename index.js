b8983d05768001a8e1d85e8618978470d5cf055f
princearman777 <sarkeraman25@gmil.com>
2023-10-21 22:38:15 +0600

    upadate product when upadate client site and show this product client site

--- index.js
+++ index.js
@@ -42,6 +42,47 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/AddCart", async (req, res) => {
+      const Product = req.body;
+      console.log(Product);
+      const result = await MyCollection.insertOne(Product);
+      console.log(result);
+      res.send(result);
+    });
+    app.get("/AddProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const quary = { _id: new ObjectId(id) };
+      const result = await ProductCollection.findOne(quary);
+      res.send(result);
+    });
+
+    app.put("/AddProduct/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = req.body;
+      console.log(updatedProduct);
+
+      const Product = {
+        $set: {
+          name: updatedProduct.name,
+          img: updatedProduct.img,
+          BrandName: updatedProduct.BrandName,
+          Price: updatedProduct.Price,
+          Type: updatedProduct.Type,
+          ShortDescription: updatedProduct.ShortDescription,
+          Ratting: updatedProduct.Ratting,
+        },
+      };
+      const result = await ProductCollection.updateOne(
+        filter,
+        Product,
+        options
+      );
+      console.log(result);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
