6193b47ce58b0f6aca28cc43bf6e0261d6d3b01d
princearman777 <sarkeraman25@gmil.com>
2023-10-21 22:24:37 +0600

    crate a Add Product in client site

--- index.js
+++ index.js
@@ -8,7 +8,68 @@ const port =process.env.PORT||5000
 app.use(cors());
 app.use(express.json());
 
+// assignment-ten
+// 3BxN00oIlSHSxicx
 
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://assignment-ten:3BxN00oIlSHSxicx@cluster0.ninjyyh.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+ 
+    // await client.connect();
+    const ProductCollection = client.db("productDb").collection("product");
+    const MyCollection = client.db("myCartDb").collection("cart");
+
+
+    app.post("/AddProduct", async(req,res) => {
+      const Product = req.body;
+        // console.log(Product);
+      const result = await ProductCollection.insertOne(Product);
+      console.log(result);
+      res.send(result);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
